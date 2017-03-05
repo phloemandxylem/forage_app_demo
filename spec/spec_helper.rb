29809081d502ac61e296caf3af57fcdd98c33461
Frances <mynameislucah@yahoo.com>
2017-03-05 13:52:31 -0600

    botanopedia commit-1

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rspec/rails'
+
+ RSpec.configure do |config|
+       config.include Devise::Test::ControllerHelpers, type: :controller
+       config.include Devise::Test::ControllerHelpers, type: :view
+    ...
+  end
